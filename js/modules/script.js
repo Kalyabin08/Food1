4e7c40aace4e3955cbdfc065a5706cf57c4c51c4
Kalyabin08 <89119857+Kalyabin08@users.noreply.github.com>
2021-08-26 13:53:49 +0500

    Add files via upload

---
+++ js/modules/script.js
@@ -0,0 +1,31 @@
+
+/* TABS 
+================================================================================*/
+import tabs  from'./modules/tabs';
+import modal  from'./modules/modal';
+import timer  from'./modules/timer';
+import cards  from'./modules/cards';
+import calc  from'./modules/calculator';
+import forms  from'./modules/forms';
+import slider  from'./modules/slider';
+
+window.addEventListener('DOMContentLoaded', () => {
+          
+      slider();
+        tabs();
+        modal();
+        timer();
+        cards();
+        calc();
+        forms();
+       
+
+});
+
+
+ 
+
+
+
+
+
